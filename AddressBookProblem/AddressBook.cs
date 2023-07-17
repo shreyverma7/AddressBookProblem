1772e668acfe27b88e74fa1ad48bf056c3d7d054
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-07-17 11:19:46 +0530

    [Shrey] Add : Ability to edit contact

--- AddressBookProblem/AddressBook.cs
+++ AddressBookProblem/AddressBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,51 @@ namespace AddressBookProblem
             };
             Console.WriteLine(contact.FirstName + "\n " + contact.LastName + "\n " + contact.Address + "\n " + contact.City + "\n " + contact.State + "\n " + contact.Zip+ "\n " + contact.PhoneNumber + "\n " + contact.Email);
             addressBook.Add(contact);
+           
+        }
+        public void EditContact(string name)
+        {
+            foreach (var contact in addressBook)
+            {
+                if(contact.FirstName.Equals(name) || contact.LastName.Equals(name))
+                {
+                    Console.WriteLine("Enter the option to Edit -->  \n1.Last name \n2.Address \n3.City Name \n4.State Name \n.5.Zip code \n6.Phone Number \n7.Email Address ");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch(option)
+                    {
+                        case 1: contact.LastName = Console.ReadLine();
+                            break;
+                        case 2:
+                            contact.Address = Console.ReadLine();
+                            break;
+                        case 3:
+                            contact.City = Console.ReadLine();
+                            break;
+                        case 4:
+                            contact.State = Console.ReadLine();
+                            break;
+                        case 5:
+                            contact.Zip = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 6:
+                            contact.PhoneNumber = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 7:
+                            contact.Email = Console.ReadLine();
+                            break;
+                    }
+                }
+                else { Console.WriteLine("Not name found"); }
+            }
+        }
+
+        public void Display()
+        {
+            foreach (var data in addressBook)
+            {
+                Console.WriteLine("View Details \n");
+                Console.WriteLine(data.FirstName+"\n"+data.LastName+"\n"+data.Address+"\n"+data.City+"\n"+data.State+"\n"+data.Zip+"\n"+data.PhoneNumber+"\n"+data.Email+"\n");
+            }
         }
     }
 }
--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -4,8 +4,34 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+
         Console.WriteLine("Welcome to Address book Problem :");
-        AddressBook book = new AddressBook();
-        book.CreateContact();
+        bool flag = true;
+        AddressBook addressBook = new AddressBook();
+        while (flag)
+        {
+             Console.WriteLine("Enter the option to proceed \n1. Create Contact \n2. Edit Details \n3. View \n4. Exit");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Creating contact : -->\n");
+                    addressBook.CreateContact();
+                    break;
+                case 2: 
+                    Console.WriteLine("Enter name to Edit Contact details\n");
+                    string input = Console.ReadLine();
+                    addressBook.EditContact(input);
+                    break;
+                case 3:
+                    addressBook.Display();
+                    break;
+
+                case 4: 
+                    flag = false;
+                    break;
+            }
+        }
+
     }
 }
